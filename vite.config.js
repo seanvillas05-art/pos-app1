02795b4fc419ed3ad6f3b8f8e2415b07c59b8715
seanvillas05-art <seanvillas05@gmail.com>
2025-08-27 17:00:09 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pos-app1/',   // must match your repo name
+  base: '/pos-app1/',   // 👈 must match repo name exactly
 })
