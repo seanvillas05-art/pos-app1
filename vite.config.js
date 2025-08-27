75a4f4767dce369e33ae9a9310b92f1cee6bd5c5
seanvillas05-art <seanvillas05@gmail.com>
2025-08-27 15:48:02 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: use your repo name here
 export default defineConfig({
   plugins: [react()],
-  base: '/pos-app1/', // <-- because your repo is pos-app1
+  base: '/pos-app1/',   // must match your repo name
 })
